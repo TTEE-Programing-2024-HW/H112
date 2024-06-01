45b3678a92e8fe20a09287d9f4273b742d198f3c
yaanho <167952003+yaanho@users.noreply.github.com>
2024-06-01 19:03:09 +0800

    h31031p3vup y

--- hw.c
+++ hw.c
@@ -3,7 +3,7 @@
 
 #define MAX_STUDENTS 10
 
-// 定義學生結構
+//定義student
 struct Student {
     char name[50];
     int id;
@@ -12,12 +12,14 @@ struct Student {
     int englishScore;
 };
 
-void inputStudentData(Student students[], int n);
+struct Student students[MAX_STUDENTS];
+int n = 0;
 
+void inputStudentData(struct Student students[], int n);
 
 int main()
 {
-// No1
+    // No1
     printf("  000000000                     4444               000000000                77777777777777777777             \n");
     printf(" 00000000000                   44444              00000000000               77777777777777777777             \n");
     printf("0000000000000                 444444             0000000000000              7777            7777             \n");
@@ -42,43 +44,39 @@ int main()
     printf(" 00000000000                     444              00000000000                 7777                           \n");
     printf("  000000000                      444               000000000                  7777                           \n");
     printf("\n");
-
+    
     system("cls");
-
+    
 // No2
-    printf("------------[Grade System]------------\n");
-    printf("|   a. Enter student grades          |\n");
-    printf("|   b. Display students grade        |\n");
-    printf("|   c. Search for student grade      |\n");
-    printf("|   d. Grade ranking                 |\n");
-    printf("|   e. Exit system                   |\n");
-    printf("--------------------------------------\n");
-    char ch;
-    printf("請輸入您的選擇(a-e)：");
-    scanf("%c", &ch);
-    
-    
-    switch(ch)
-    {
-            
+    while (1) {
+        printf("------------[Grade System]------------\n");
+        printf("|   a. Enter student grades          |\n");
+        printf("|   b. Display students grade        |\n");
+        printf("|   c. Search for student grade      |\n");
+        printf("|   d. Grade ranking                 |\n");
+        printf("|   e. Exit system                   |\n");
+        printf("--------------------------------------\n");
+        char ch;
+        printf("請輸入您的選擇(a-e)：");
+        scanf(" %c", &ch);
+        
+        switch(ch)
+        {
+            case 'a': // enter a or A
+            case 'A':
+                
 // No3
-        case 'a': //enter a or A
-        case 'A':
-            system("cls"); //clear
-            
-            
-            Student students[MAX_STUDENTS];
+                system("cls"); // clear
+                printf("請輸入數字(5-10)：");
+                scanf("%d", &n);
 
-            int n;
-            printf("請輸入數字(5-10)：");
-            scanf("%d", &n);
-            if(n>10||n<5)
-            {
-                printf("錯誤請重新輸入!");
-                break;
-            }
-          
-                for (int i = 0; i < n; ++i) //有幾個學生就輸入多少資料
+                if(n > 10 || n < 5)
+                {
+                    printf("錯誤請重新輸入!\n");
+                    break;
+                }
+
+                for (int i = 0; i < n; ++i) // 有幾個學生就輸入多少資料
                 {
                     printf("學生 %d 的姓名: ", i + 1);
                     scanf("%s", students[i].name);
@@ -96,16 +94,49 @@ int main()
                     scanf("%d", &students[i].englishScore);
                     
                     // 檢查成績是否合法
-                    if (students[i].mathScore < 0 || students[i].mathScore > 100 ||students[i].physicsScore < 0 || students[i].physicsScore > 100 ||students[i].englishScore < 0 || students[i].englishScore > 100)
+                    if (students[i].mathScore < 0 || students[i].mathScore > 100 ||students[i].physicsScore < 0 || students[i].physicsScore > 100 ||students[i].englishScore < 0 ||students[i].englishScore > 100)
                     {
                         printf("錯誤：請輸入有效的成績範圍 (0~100)。\n");
-                        --i;
+                        --i; // 重新输入当前学生的资料
                     }
-
                 }
-            break;
-        }
+                break;
+                
+// No4
+                
+            case 'b':
+            case 'B':
+                
+        
+                system("cls");
+                
+                if (n == 0) {
+                    printf("目前沒有學生資料。\n");
+                } else {
+                    printf("學生資料：\n");
+                    for (int i = 0; i < n; ++i) {
+                        printf("姓名: %s\n", students[i].name);
+                        printf("學號: %d\n", students[i].id);
+                        printf("數學成績: %d\n", students[i].mathScore);
+                        printf("物理成績: %d\n", students[i].physicsScore);
+                        printf("英文成績: %d\n", students[i].englishScore);
+                        float average = (students[i].mathScore + students[i].physicsScore + students[i].englishScore) / 3.0f;
+                        printf("平均成績: %.1f\n\n", average);
+                    }
+                }
+                printf("按任意鍵回到主選單...");
+                getchar(); // 等待用戶按下任意鍵
+                getchar(); // 捕獲上一次 getchar() 後的換行符號
+                break;
+                
+       
+                
             
+        }
+    }
+    
+    return 0;
+}
             
     }
 
