a235c27ea6b994f22c6a7b5b41d89d46c0566bbe
yaanho <167952003+yaanho@users.noreply.github.com>
2024-06-01 18:43:09 +0800

    新增選項a 讀入學生資料

--- hw.c
+++ hw.c
@@ -12,6 +12,8 @@ struct Student {
     int englishScore;
 };
 
+void inputStudentData(Student students[], int n);
+
 
 int main()
 {
@@ -51,6 +53,60 @@ int main()
     printf("|   d. Grade ranking                 |\n");
     printf("|   e. Exit system                   |\n");
     printf("--------------------------------------\n");
+    char ch;
+    printf("請輸入您的選擇(a-e)：");
+    scanf("%c", &ch);
+    
+    
+    switch(ch)
+    {
+            
+// No3
+        case 'a': //enter a or A
+        case 'A':
+            system("cls"); //clear
+            
+            
+            Student students[MAX_STUDENTS];
 
+            int n;
+            printf("請輸入數字(5-10)：");
+            scanf("%d", &n);
+            if(n>10||n<5)
+            {
+                printf("錯誤請重新輸入!");
+                break;
+            }
+          
+                for (int i = 0; i < n; ++i) //有幾個學生就輸入多少資料
+                {
+                    printf("學生 %d 的姓名: ", i + 1);
+                    scanf("%s", students[i].name);
+                    
+                    printf("學號 (6位整數): ");
+                    scanf("%d", &students[i].id);
+                    
+                    printf("數學成績 (0~100): ");
+                    scanf("%d", &students[i].mathScore);
+                    
+                    printf("物理成績 (0~100): ");
+                    scanf("%d", &students[i].physicsScore);
+                    
+                    printf("英文成績 (0~100): ");
+                    scanf("%d", &students[i].englishScore);
+                    
+                    // 檢查成績是否合法
+                    if (students[i].mathScore < 0 || students[i].mathScore > 100 ||students[i].physicsScore < 0 || students[i].physicsScore > 100 ||students[i].englishScore < 0 || students[i].englishScore > 100)
+                    {
+                        printf("錯誤：請輸入有效的成績範圍 (0~100)。\n");
+                        --i;
+                    }
+
+                }
+            break;
+        }
+            
+            
+    }
 
 
