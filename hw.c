30d450c9acda930fd0a6e020293708a9b4337fd7
yaanho <167952003+yaanho@users.noreply.github.com>
2024-06-01 18:18:22 +0800

    此版本新增主螢幕（第二題）

--- hw.c
+++ hw.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+// No1
     printf("  000000000                     4444               000000000                77777777777777777777             \n");
     printf(" 00000000000                   44444              00000000000               77777777777777777777             \n");
     printf("0000000000000                 444444             0000000000000              7777            7777             \n");
@@ -28,6 +29,17 @@ int main()
     printf(" 00000000000                     444              00000000000                 7777                           \n");
     printf("  000000000                      444               000000000                  7777                           \n");
     printf("\n");
+
+    system("cls");
+
+// No2
+    printf("------------[Grade System]------------\n");
+    printf("|   a. Enter student grades          |\n");
+    printf("|   b. Display students grade        |\n");
+    printf("|   c. Search for student grade      |\n");
+    printf("|   d. Grade ranking                 |\n");
+    printf("|   e. Exit system                   |\n");
+    printf("--------------------------------------\n");
 
 
 
